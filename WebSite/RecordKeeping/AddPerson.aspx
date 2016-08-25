a10355becc9389ed2150d0c4b012e562f7367b48
dmerson <dmerson@email.arizona.edu>
2016-08-25 15:38:21 -0700

    Add person is working.

---
+++ WebSite/RecordKeeping/AddPerson.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddPerson.aspx.cs" Inherits="RecordKeeping.AddPerson" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <div class="row">
+        <span class="col-lg-2">
+            First Name
+        </span>
+        <span class="col-lg-10">
+            <asp:TextBox runat="server" id="txtFirstName"></asp:TextBox>
+</span>
+    </div>
+    <div class="row">
+        <span class="col-lg-2">
+            Last Name
+        </span>
+        <span class="col-lg-10">
+            <asp:TextBox runat="server" id="txtLastName"></asp:TextBox>
+</span>
+    </div>
+    <div class="row">
+        <span class="col-lg-2">
+            State
+        </span>
+        <span class="col-lg-10">
+            <asp:TextBox runat="server" id="txtState"></asp:TextBox>
+</span>
+        <div class="row">
+        <span class="col-lg-2">
+           
+        </span>
+        <span class="col-lg-10">
+            <asp:Button runat="server" id="btnSave" text="Quick Save" OnClick="btnSave_Click"/>
+</span>
+    </div>
+        <asp:Label runat="server" id="lblPerson"></asp:Label>
+    </div>
+</asp:Content>
---
+++ WebSite/RecordKeeping/AddPerson.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RecordKeeping
+{
+    public partial class AddPerson : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var db = new ExamplesDBEntities())
+                {
+                    var result =
+                        db.InsertPerson(this.txtFirstName.Text, this.txtLastName.Text, null, null, null,
+                            this.txtState.Text, null).FirstOrDefault();
+                    if (result == null)
+                    {
+                        this.lblPerson.Text = "There is a problem";
+                    }
+                    else
+                    {
+                        this.lblPerson.Text = $"Your Is is {result.PersonId }";
+                       
+                    }
+                
+                }
+            }
+            catch (Exception exception)
+            {
+                this.lblPerson.Text = $"There was a problem with the database {exception.Message}";
+            }
+        }
+    }
+}
